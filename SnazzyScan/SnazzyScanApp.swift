52b1d82ad72ea93dc943ee72246de24bee9fffdb
Michael Fluharty <michael.fluharty@mac.com>
2025-10-05 09:50:17 -0500

    Initial Commit

---
+++ SnazzyScan/SnazzyScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnazzyScanApp.swift
+//  SnazzyScan
+//
+//  Created by Michael Fluharty on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SnazzyScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnazzyScanTests/SnazzyScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnazzyScanTests.swift
+//  SnazzyScanTests
+//
+//  Created by Michael Fluharty on 10/5/25.
+//
+
+import Testing
+@testable import SnazzyScan
+
+struct SnazzyScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
